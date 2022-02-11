376a09d0bef67dbf5d89b630a01b27d114b39e45
Jukibow <oj121125@gmail.com>
2022-02-11 20:16:32 +0900

    003: 003 - Longest Circular Road（★4）

---
+++ training/003.py
@@ -0,0 +1,45 @@
+import sys
+
+sys.setrecursionlimit(200000)
+
+
+def main():
+
+    N = int(input())
+
+    # 初期化
+    first_diagram = [[0]*N for i in range(N)]
+    second_diagram = [[0]*N for i in range(N)]
+    # 経路を登録
+    for n in range(N - 1):
+        A, B = input().split()
+        A, B = int(A), int(B)
+        first_diagram[A-1][B-1] = 1
+        first_diagram[B-1][A-1] = 1
+        second_diagram[A-1][B-1] = 1
+        second_diagram[B-1][A-1] = 1
+
+    u = 0
+    v = 0
+
+    _, u = bft(first_diagram, u, 0, u, 0)
+    max, _ = bft(second_diagram, u, 0, v, 0)
+    print(max+1)
+
+
+def bft(diagram, current, depth, end, max_depth):
+
+    for a in range(len(diagram[current])):
+        if diagram[current][a] == 1:
+            diagram[current][a] = 0
+            diagram[a][current] = 0
+            next_depth = depth + 1
+            if next_depth > max_depth:
+                max_depth = next_depth
+                end = a
+            max_depth, end = bft(diagram, a, next_depth, end, max_depth)
+    return max_depth, end
+
+
+if __name__ == "__main__":
+    main()
