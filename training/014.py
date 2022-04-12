8da1dffc738541650ccc93e45df1858492c7438f
Jukibow <oj121125@gmail.com>
2022-04-12 21:12:18 +0900

    014	We Used to Sing a Song Together（★3）

---
+++ training/014.py
@@ -0,0 +1,19 @@
+def main():
+    # 入力
+    N = int(input())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    A.sort()
+
+    B.sort()
+
+    result = 0
+    for i in range(N):
+        result += abs(A[i] - B[i])
+
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
